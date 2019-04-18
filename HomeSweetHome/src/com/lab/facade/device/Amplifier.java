a4affb65f7d9f6b724fe53bc8535305ce1ace08c
Marcin <mrn.woloszyn@gmail.com>
2019-04-18 20:50:11 +0200

    dodalem dvdplayer do amplifier

--- HomeSweetHome/src/com/lab/facade/device/Amplifier.java
+++ HomeSweetHome/src/com/lab/facade/device/Amplifier.java
@@ -3,6 +3,7 @@ package com.lab.facade.device;
 public class Amplifier implements Device{
 
 	 public Tuner tuner;
+	 public DvdPlayer dvdplayer;
 	
 	 
 	public  void setStereoSound() {
@@ -15,6 +16,7 @@ public class Amplifier implements Device{
 	public void on() {
 		// TODO Auto-generated method stub
 		tuner.on();
+		dvdplayer.on();
 	
 		
 	}
@@ -24,6 +26,7 @@ public class Amplifier implements Device{
 	public void off() {
 		// TODO Auto-generated method stub
 		tuner.off();
+		dvdplayer.on();
 		
 		
 	}
